b4f713f3f5ab0f9979039372844db11d47db6188
Luiz Carlos Leão <18469965+lcsl88@users.noreply.github.com>
2022-03-08 17:33:47 -0300

    Add files via upload

---
+++ Aula8-08112021/Remove-naoordenada-luiz.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+void Insere(int V[], int N, int x, int p); 
+// V-> Vetor; N -> Numero de elementos; x-> elemento a ser inserido; p-> posição do elemento
+
+int Busca(int V[], int N, int x);
+
+void Remove(int V[], int N, int x);
+
+int main(){
+	//lista linear
+	//não-ordenada
+	//sequencial
+	int V[11];
+	int N = 0;
+	Insere(V,0,3,1); // Considerei o numero N = 0 de elementos inicial do vetor 
+	Insere(V,1,4,2); // Agora considerei o vetor com N = 1 elemento
+	Insere(V,2,5,1); // Por fim com N = 2 elementos
+	//cout << Busca(V,3,5) << "\n"; //1
+	//cout << Busca(V,3,3) << "\n"; //3
+	//cout << Busca(V,3,7) << "\n"; //-1
+	cout << V[1] << V[2] << V[3] << "\n"; // SAIDA: 543
+	Remove(V,3,4);
+	cout << V[1] << V[2];
+	
+	//cout << V[1]; // SAIDA: 3
+	//cout << V[1] << V[2]; // SAIDA: 34
+	//cout << V[1] << V[2] << V[3]; // SAIDA: 543
+	return 0;
+}
+
+void Insere(int V[], int N, int x, int p){
+	V[N+1]=V[p];
+	V[p]=x;
+	N=N+1;
+}
+
+// Solução Fabiano no vídeo da Aula 08
+int Busca(int V[], int N, int x){
+	int i = 1;
+	while ((i <= N) && (V[i] != x)){
+		i = i + 1;
+ 	}
+	if (i > N){
+		return -1;
+	} else {
+		return i;
+	}
+}
+
+// Solução minha errada (11/11/2021 - 19:30)
+// int Busca(int V[], int N, int x){
+//	int i = 1;
+//	while(i<=N && V[i]!=x){
+//		i=i+1;
+//		if(i>N){
+//			return -1;
+//		}
+//		else{
+//			return i;
+//		}
+//	}
+//}
+
+void Remove(int V[], int N, int x){
+	int i = 1;
+	while(V[i]!=x){
+		i=i+1;
+	}
+	V[i]=V[N];
+	N=N-1;
+}
